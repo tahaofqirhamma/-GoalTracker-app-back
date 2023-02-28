381e90e3bbf649766a030d11df14964f8eedbd72
tahaofqirhamma <taha_ofqirhamma@um5.ac.ma>
2023-02-28 08:27:47 +0100

    fixing bags

--- api/Tasks.php
+++ api/Tasks.php
@@ -1,18 +1,21 @@
 <?php
 
 include "../database/index.php";
-
-
-$query = "SELECT * FROM task WHERE task_owner = :test";
-$res = $conn->prepare($query);
-$res->execute(['test' => 14]);
-$rows = $res->fetchAll();
-if ($res->rowCount() > 0) {
-    foreach ($rows as $row) {
-        $task = array('tn' => $row['tname'], 'tdesc' => $row['tdescription'], 'td' => $row['tdate'], 'ts' => $row['tstate'], 'tnum' => $row['num_task']);
-        $tasks[] = $task;
+if (isset($_POST['userID'])) {
+    $userID = $_POST['userID'];
+    $query = "SELECT * FROM task WHERE task_owner = :test";
+    $res = $conn->prepare($query);
+    $res->execute(['test' => $userID]);
+    $rows = $res->fetchAll();
+    if ($res->rowCount() > 0) {
+        foreach ($rows as $row) {
+            $task = array('tn' => $row['tname'], 'tdesc' => $row['tdescription'], 'td' => $row['tdate'], 'ts' => $row['tstate'], 'tnum' => $row['num_task']);
+            $tasks[] = $task;
+        }
+        echo json_encode($tasks);
+    } else {
+        echo json_encode(['message' => 'no tasks found']);
     }
-    echo json_encode($tasks);
 } else {
-    echo json_encode(['message' => 'no tasks found']);
+    echo "there's no userid";
 }
